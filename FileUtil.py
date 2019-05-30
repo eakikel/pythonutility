4f53d33fd35c5fa150476a7d517ccbfa89a8cf07
erich kikel <erich@kikel.org>
2019-05-30 16:17:59 -0400

    Update

---
+++ FileUtil.py
@@ -0,0 +1,13 @@
+import pathlib
+import os, sys
+
+def DeleteOldFiles(path,days,pattern)
+# define the path
+currentDirectory = pathlib.Path('.')
+
+for currentFile in currentDirectory.iterdir():  
+  if os.path.isdir(currentFile):
+    print("It is a Directory-{0}".format(currentFile))
+
+  if os.path.isfile(currentFile):  
+    print("It is a normal file-{0}".format(currentFile))  
